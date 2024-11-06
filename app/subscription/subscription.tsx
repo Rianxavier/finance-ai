91756347a6af43daaccdc92c44c61940b77043dd
Rian Xavier <rianalvesxvr@gmail.com>
2024-11-06 02:22:26 -0300

    feat: add login page

--- app/subscription/subscription.tsx
+++ app/subscription/subscription.tsx
@@ -1,7 +1,7 @@
-const Subscription = () => {
+const SubscriptionPage = () => {
     return (
         <div>Subscription</div>
     )
 }
 
-export default Subscription
+export default SubscriptionPage
