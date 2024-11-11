c9f1e0327b6bbd6608fc9dd50e5b4161521e1f03
Rian Xavier <rianalvesxvr@gmail.com>
2024-11-11 19:56:48 -0300

    feat: add last transactions component

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -7,6 +7,7 @@ import { isMatch } from "date-fns";
 import { getDashboard } from "../_data/get-dashboard";
 import TransactionsPieChart from "./_components/transactions-pie-chart";
 import ExpensesPerCategory from "./_components/expenses-per-category";
+import LastTransaction from "./_components/last-transactions";
 
 interface HomeProps {
   searchParams: {
@@ -31,7 +32,7 @@ const Home = async ({ searchParams: { month } }: HomeProps) => {
           <h1 className="text-2xl font-bold">Dashboard</h1>
           <TimeSelect />
         </div>
-        <div className="grid grid-cols-[2fr,1fr]">
+        <div className="grid grid-cols-[2fr,1fr] gap-6">
           <div className="flex flex-col gap-6">
             <SummaryCards month={month} {...dashboard} />
 
@@ -42,6 +43,7 @@ const Home = async ({ searchParams: { month } }: HomeProps) => {
               />
             </div>
           </div>
+          <LastTransaction lastTransactions={dashboard.lastTransactions} />
         </div>
       </div>
     </>
